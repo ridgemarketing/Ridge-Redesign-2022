1286f469cad8428aec05f8a4c7dbf8598ca1e0b7
Nicolas Borges <nicolasb@nicolass-mbp.mynetworksettings.com>
2022-06-03 10:52:21 -0400

    wrapper components and initial flex layout setup

---
+++ src/layouts/page/FullWidthTextImage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FullWidthTextImage = (props) => {
+    return (
+        <h1>This is the content for my full width text/image component</h1>
+    )
+}
+
+export default FullWidthTextImage
