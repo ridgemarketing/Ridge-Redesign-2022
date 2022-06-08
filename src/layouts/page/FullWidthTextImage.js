3803b19ec405100b0f0ad2bfe5d542f0451619bd
Nicolas Borges <nicolasb@Nicolass-MBP.localdomain>
2022-06-08 14:31:57 -0400

    changes to pages and gatsby node

--- src/layouts/page/FullWidthTextImage.js
+++ src/layouts/page/FullWidthTextImage.js
@@ -3,6 +3,9 @@ import {Section, Container } from "../../components/global/Wrappers"
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { theme } from "../../static/theme"
 
+//USE THIS SECTION FOR TESTING SECTION STYLES
+//any code put on this page will render to /sample-page
+
 const FullWidthTextImage = (props) => {
 
     const content = props.layoutData.layoutContent;
