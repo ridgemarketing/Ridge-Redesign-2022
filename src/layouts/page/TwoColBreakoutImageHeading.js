999bbcb97d30a42aa1ffa620e08a9ec019d8cf57
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2022-07-12 15:48:13 -0400

    update: breakout img heading

--- src/layouts/page/TwoColBreakoutImageHeading.js
+++ src/layouts/page/TwoColBreakoutImageHeading.js
@@ -7,21 +7,27 @@ import { graphql } from "gatsby"
 const TwoColBreakoutImageHeading = (props) => {
     const content = props.layoutData.layoutContent;
     const settings = props.layoutData.layoutSettings;
-    const image = getImage(content.image.localFile)
+    const image = getImage(content.image.localFile);
+
+    const bgColor = settings.backgroundColor ? settings.backgroundColor : `white`;
 
     return (
-        <Section settings={settings} classes={'2xl:max-w-[1920px] 2xl:mx-auto'}>
-            <div className={'px-6 order-2 mt-16 mx-auto max-w-[812px] xl:absolute xl:mt-0 xl:mx-0 xl:right-0 xl:top-0 '}>
-              <GatsbyImage image={image} />
-            </div>
-            <Container>
-                <div className={'flex justify-start xl:mt-20 xl:mb-[600px] order-1 relative z-10'}>
-                    <div className={'flex-1 px-5 xl:pl-10'}>
-                        <h2>
-                            <span className={theme.text.H2}>{content.heading}</span>
+        <Section settings={settings} classes={`overflow-hidden relative`}>
+            <div className={`bg-white absolute top-0 left-0 right-0 w-full h-32`}></div>
+            <div className={`bg-${bgColor} absolute top-32 left-0 right-0 bottom-0 w-full h-full`}></div>
+            <Container size={`slim`}>
+              <div className={`relative xl:min-h-[510px]`}>
+                <div className={`lg:absolute lg:top-0 max-h-[510px] max-w-[810px] lg:left-[calc(50%+2.25rem)] lg:w-[calc(50%+50vw-465px)] xl:left-[calc(655px+3.5rem)] xl:w-[calc(655px+50vw-640px)]`}>
+                  <GatsbyImage image={image} />
+                </div>
+                <div className={`py-16 lg:pt-48 lg:flex xl:pt-64`}>
+                    <div className={`lg:w-1/2 lg:mr-9 xl:mr-14 xl:w-[655px]`}>
+                        <h2 className={theme.text.H1_STD}>
+                            {content.heading}
                         </h2>
                     </div>
-                    <div className={`hidden xl:block xl:w-[calc(726px-(50vw-640px))] 2xl:w-[calc(726px-(50vw-640px)+(50vw-960px))] ml-8`}></div>
+                    <div className={`lg:max-w-1/2`}></div>
+                </div>
                 </div>
             </Container>
         </Section>
