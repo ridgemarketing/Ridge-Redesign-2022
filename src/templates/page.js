cf1aa08c3e342a44e32f7ce3b555f174324fab50
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2022-07-12 15:50:14 -0400

    add: page header

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,12 +1,16 @@
 import React, { useRef, useEffect, useState } from "react"
 import { graphql } from "gatsby"
 import FlexibleLayouts from "../layouts/FlexibleLayouts"
+import PageHeader from "../layouts/page/PageHeader"
 
 const WpPage = ({ data }) =>{
   return (
     <div>
       {/* <h1> {data.wpPage.title} </h1> */}
       <p> {data.wpPage.content} </p>
+      {data.wpPage.pageHeader &&
+        <PageHeader layoutContent={data.wpPage.pageHeader.pageHeader} />
+      }
       <div>
           <FlexibleLayouts flexibleLayouts={data.wpPage.flexibleLayouts} />
       </div>
@@ -17,14 +21,29 @@ export default WpPage;
 
 
 export const query = graphql`
-query PageById($id: String) {
-  wpPage(id: {eq: $id}) {
-    id
-    uri
-    title
-    content
-    ...FlexibleLayoutsPage
-  }
-}
-
-` 
+  query PageById($id: String) {
+    wpPage(id: {eq: $id}) {
+      id
+      uri
+      title
+      content
+      ...FlexibleLayoutsPage
+      pageHeader {
+        pageHeader {
+          eyebrow
+          heading
+          maxWidth
+          reverseHeading
+          subheading
+          backgroundImage {
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  } 
+`
