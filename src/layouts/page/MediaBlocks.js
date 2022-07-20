bc9caf4824d0f727a4ac5376ded4da2be55e30b0
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2022-07-20 16:36:17 -0400

    add: media blocks

--- src/layouts/page/MediaBlocks.js
+++ src/layouts/page/MediaBlocks.js
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { Component } from "react"
 import { graphql } from "gatsby"
+import { Section, Container } from "../../components/global/Wrappers"
+import { theme } from "../../static/theme"
+import FlexibleMedia from "../../components/global/FlexibleMedia"
 
-const MediaBlocks = () => {
+const MediaBlocks = (props) => {
+  const content   = props.layoutData.layoutContent;
+  const settings  = props.layoutData.layoutSettings;
+
+  const media     = content.blocks
+
     return(
-      <h1>Contact 4 square</h1>
+      <Section settings={settings}>
+        <Container>
+          {content.heading &&
+            <h2 className={`text-center mb-14 ${theme.text.H2}`}>{content.heading}</h2>
+          }
+          {media &&
+            <div className={`lg:flex flex-wrap items-center lg:-mx-10`}>
+              {media.map(block => {
+                return (
+                  <div className={`lg:w-[calc(50%-5rem)] mb-20 lg:mx-10`}>
+                    <FlexibleMedia data={block.componentFlexibleMedia} />
+                  </div>
+                )
+              })}
+            </div>
+          }
+        </Container>
+      </Section>
     )
 }
 
