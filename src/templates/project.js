2cdb12cf6308c22509b354f76ab2fbebd6b0ce9d
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2022-07-20 15:57:16 -0400

    add: project header to project

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,13 +1,21 @@
 import React from "react"
 import { graphql } from "gatsby"
 import FlexibleLayouts from "../layouts/FlexibleLayouts"
+import ProjectHeader from "../layouts/page/ProjectHeader"
 
 const WpProject = ({ data }) =>{
   return (
     <div>
-      <div>
-          <FlexibleLayouts flexibleLayouts={data.wpProject.flexibleLayouts} />
-      </div>
+        {data.wpProject && 
+          <div>
+            <ProjectHeader content={data.wpProject.projectHeader} info={data.wpProject.projectInformation} />
+          </div>
+        }
+      {data.wpProject.flexibleLayouts && 
+        <div>
+            <FlexibleLayouts flexibleLayouts={data.wpProject.flexibleLayouts} />
+        </div>
+      }
     </div>
   )
 }
@@ -22,6 +30,7 @@ export const query = graphql`
       title
       content
       ...FlexibleLayoutsProject
+      ...ProjectHeader
     }
   }
 ` 
