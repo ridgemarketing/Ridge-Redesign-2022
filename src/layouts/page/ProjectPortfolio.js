b763c7b557b635646ea5a721c893e982dc724a23
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2022-07-20 15:54:03 -0400

    add: flexibleMedia

--- src/layouts/page/ProjectPortfolio.js
+++ src/layouts/page/ProjectPortfolio.js
@@ -1,9 +1,123 @@
-import React from "react"
+import React, {useState, useRef, useEffect, forwardRef} from "react"
 import { graphql } from "gatsby"
+import { Section, Container } from "../../components/global/Wrappers"
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { theme } from "../../static/theme"
 
-const ProjectPortfolio = () => {
+
+const ProjectPortfolioImage = forwardRef((props, ref) => {
+
+  const { startRef, endRef } = ref.current
+  const mobile = props.image.mobile ? getImage(props.image.mobile.localFile) : false
+  const tablet = props.image.tablet ? getImage(props.image.tablet.localFile) : false
+  const desktop = props.image.desktop ? getImage(props.image.desktop.localFile) : false
+
+  let desktopClassName  = `text-center `
+  let tabletClassName   = `text-center `
+  let mobileClassName   = `text-center `
+
+  let first             = false
+  let last              = false
+
+  if (desktop) {
+    desktopClassName  += ``
+    tabletClassName   += `xl:hidden `
+    mobileClassName   += `xl:hidden `
+  }
+
+  if (tablet) {
+    desktopClassName  += `md:hidden `
+    tabletClassName   += ``
+    mobileClassName   += `md:hidden `
+  }
+
+  if (mobile) {
+    desktopClassName  += `hidden xl:block `
+    tabletClassName   += `hidden md:block `
+    mobileClassName   += ``
+  }
+
+  if (props.index === 0) {
+    first = true
+  }
+
+  if (props.index === (props.length - 1)) {
+    last = true
+  }
+
+  return (
+    <div className={`portfolio-image relative`}>
+      
+      {first &&
+        <div ref={startRef} className={`portfolio-start absolute top-[12.5%]`}></div>
+      }
+
+      {last &&
+        <div ref={endRef} className={`portfolio-end absolute ${props.length === 1 ? `bottom-[12.5%]` : `bottom-[12.5%] xl:bottom-2/3`}`}></div>
+      }
+
+      {mobile &&
+        <div className={`${mobileClassName}`}>
+          <GatsbyImage image={mobile} />
+        </div>
+      }
+      {tablet &&
+        <div className={`${tabletClassName}`}>
+          <GatsbyImage image={tablet} />
+        </div>
+      }
+      {desktop &&
+        <div className={`${desktopClassName}`}>
+          <GatsbyImage image={desktop} />
+        </div>
+      }
+  </div>
+  )
+})
+
+const ProjectPortfolio = (props) => {
+
+  const content   = props.layoutData.layoutContent;
+  const settings  = props.layoutData.layoutSettings;
+
+  const bgImage   = content.settings.backgroundImage ? getImage(content.settings.backgroundImage.localFile) : false
+  const images    = content.images
+
+  const startRef  = useRef(null)
+  const endRef    = useRef(null)
+  const ref       = useRef({startRef, endRef})
+
+  const [bgHeight, setBgHeight] = useState(null)
+
+  useEffect(() => {
+    function handleResize() {
+      if (startRef.current && endRef.current) {
+        setBgHeight(endRef.current.getBoundingClientRect().top - startRef.current.getBoundingClientRect().top)
+      }
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+  })
+
     return(
-        <></>
+      <Section classes={`relative`} settings={settings}>
+        {content.settings.backgroundColor && 
+          <div className={`absolute w-full`} style={{backgroundColor: content.settings.backgroundColor, height: bgHeight + `px`, top: startRef.current ? startRef.current.offsetTop : `auto` }}></div>
+        }
+
+        {bgImage && 
+          <GatsbyImage image={bgImage} objectFit={`cover`} className={`absolute w-full`} style={{height: bgHeight + `px`, top: startRef.current ? startRef.current.offsetTop : `auto` }} />
+        }
+        <Container>
+          <div className={`relative`}>
+            {images &&
+              images.map((image, index) => {
+                return <ProjectPortfolioImage ref={ref} image={image} key={index} index={index} length={images.length} />
+              })
+            }
+          </div>
+        </Container>
+      </Section>
     )
 }
 
@@ -134,21 +248,21 @@ export const projectQuery = graphql`
               desktop {
                 localFile {
                   childImageSharp {
-                    gatsbyImageData
+                    gatsbyImageData (quality: 99, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
                   }
                 }
               }
               mobile {
                 localFile {
-                  childImageSharp {
-                    gatsbyImageData
+                  childImageSharp { 
+                    gatsbyImageData (quality: 95, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
                   }
                 }
               }
               tablet {
                 localFile {
                   childImageSharp {
-                    gatsbyImageData
+                    gatsbyImageData (quality: 95, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
                   }
                 }
               }
@@ -158,7 +272,7 @@ export const projectQuery = graphql`
               backgroundImage {
                 localFile {
                   childImageSharp {
-                    gatsbyImageData
+                    gatsbyImageData (quality: 99, formats: [AUTO, WEBP, AVIF])
                   }
                 }
               }
