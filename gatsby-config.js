562dd7b29a018b9b40504986c692d1a1d679df80
Nicolas Borges <nicolasb@nicolas-mbp.mynetworksettings.com>
2022-12-16 12:58:13 -0500

    debug changes for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         production: {
-         allow404Images: true,
+          allow404Images: true,
         },
         url:
         // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
