4be9cc78dee39355039209f9585be073e27dd250
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2023-05-16 16:33:36 -0400

    add: form messaging

--- src/components/global/Forms.js
+++ src/components/global/Forms.js
@@ -5,15 +5,26 @@ import { theme } from "../../static/theme"
 
 export const FormCareers = ({formId, classes, submitLabel, btnContainerClasses, btnStyle}) => {
     const [state, handleSubmit] = useForm(formId)
-    if (state.succeeded) {
-        return (
-            <div className={`text-center text-rm-green`}>
-                <p className={`${theme.text.P_BLD}`}>Thank you for your submission. We will be in touch.</p>
-            </div>
-        );
-    }
+
     return(
         <form onSubmit={handleSubmit} className={classes}>
+
+            {state.errors[0] && 
+                <div className={`bg-[#E10000] text-white py-3 px-6 mt-3 mb-6`}>
+                    {state.errors.map((error) => {
+                        return(
+                            <p className={`${theme.text.P_BLD}`}>{error.message}</p>
+                        )
+                    })}
+                </div>
+            }
+
+            {state.succeeded &&
+                <div className={`text-center text-rm-green`}>
+                    <p className={`${theme.text.P_BLD}`}>Thank you for your submission. We will be in touch.</p>
+                </div>
+            }
+
             <span className={`block mb-2`}><Input state={state} type={`text`} name={`name`} label={`Name`} textColor={`white`} bgColor={`black`} /></span>
             <span className={`block mb-2`}><Input state={state} type={`email`} name={`email`} label={`Email`} textColor={`white`} bgColor={`black`} /></span>
             <span className={`block mb-2`}><Input state={state} type={`tel`} name={`phone`} label={`Phone`} textColor={`white`} bgColor={`black`} /></span>
@@ -34,20 +45,31 @@ export const FormCareers = ({formId, classes, submitLabel, btnContainerClasses, 
 
 export const FormContacPage = ({formId, classes, submitLabel, btnContainerClasses, btnStyle}) => {
     const [state, handleSubmit] = useForm(formId)
-    if (state.succeeded) {
-        return (
-            <div className={`text-center text-rm-green`}>
-                <p className={`${theme.text.P_BLD}`}>Thank you for your submission. We will be in touch.</p>
-            </div>
-        );
-    }
+
     return(
         <form onSubmit={handleSubmit} className={classes}>
-                            <span className={`mb-6 block`}><Input state={state} type={`text`} name={`name`} label={`Name`} bgColor={`white`} textColor={`black`} /></span>
-                <span className={`mb-6 block`}><Input state={state} type={`text`} name={`company`} label={`Company`} bgColor={`white`} textColor={`black`} /></span>
-                <span className={`mb-6 block`}><Input state={state} type={`email`} name={`email`} label={`Email`} bgColor={`white`} textColor={`black`} /></span>
-                <span className={`mb-6 block`}><Input state={state} type={`tel`} name={`phone`} label={`Phone`} bgColor={`white`} textColor={`black`} /></span>
-                <span className={`mb-6 block`}><TextArea state={state} name={`message`} label={`What are your marketing goals?`} bgColor={`white`} textColor={`black`} /></span>
+            {state.errors[0] && 
+                <div className={`bg-[#E10000] text-white py-3 px-6 mt-3 mb-6`}>
+                    {state.errors.map((error) => {
+                        return(
+                            <p className={`${theme.text.P_BLD}`}>{error.message}</p>
+                        )
+                    })}
+                </div>
+            }
+
+            {state.succeeded &&
+                <div className={`text-center text-rm-green`}>
+                    <p className={`${theme.text.P_BLD}`}>Thank you for your submission. We will be in touch.</p>
+                </div>
+            }
+
+            <span className={`mb-6 block`}><Input state={state} type={`text`} name={`name`} label={`Name`} bgColor={`white`} textColor={`black`} /></span>
+            <span className={`mb-6 block`}><Input state={state} type={`text`} name={`company`} label={`Company`} bgColor={`white`} textColor={`black`} /></span>
+            <span className={`mb-6 block`}><Input state={state} type={`email`} name={`email`} label={`Email`} bgColor={`white`} textColor={`black`} /></span>
+            <span className={`mb-6 block`}><Input state={state} type={`tel`} name={`phone`} label={`Phone`} bgColor={`white`} textColor={`black`} /></span>
+            <span className={`mb-6 block`}><TextArea state={state} name={`message`} label={`What are your marketing goals?`} bgColor={`white`} textColor={`black`} /></span>
+            
             <div className={`mt-6 ${btnContainerClasses ? btnContainerClasses : ``}`}>
                 <button
                     className={`${theme.button['BASE_STYLING']} ${theme.button[btnStyle ? btnStyle : `SOLID_GREEN_HOVER_DARK`]} cursor-pointer min-w-[210px]`}
@@ -61,21 +83,31 @@ export const FormContacPage = ({formId, classes, submitLabel, btnContainerClasse
 
 export const FormCTALayout = ({formId, classes, submitLabel, btnContainerClasses, btnStyle, textColor, bgColor}) => {
     const [state, handleSubmit] = useForm(formId)
-    if (state.succeeded) {
-        return (
-            <div className={`text-center text-rm-green`}>
-                <p className={`${theme.text.P_BLD}`}>Thank you for your submission. We will be in touch.</p>
-            </div>
-        );
-    }
     return(
         <form onSubmit={handleSubmit} className={classes}>
-                <span className={`mb-6 block md:col-span-1`}><Input state={state} type={`text`} name={`name`} label={`Name`} textColor={textColor} bgColor={bgColor} /></span>
-                <span className={`mb-6 block md:col-span-1`}><Input state={state} type={`text`} name={`company`} label={`Company`} textColor={textColor} bgColor={bgColor} /></span>
-                <span className={`mb-6 block md:col-span-1`}><Input state={state} type={`email`} name={`email`} label={`Email`} textColor={textColor} bgColor={bgColor} /></span>
-                <span className={`mb-6 block md:col-span-1`}><Input state={state} type={`tel`} name={`phone`} label={`Phone`} textColor={textColor} bgColor={bgColor} /></span>
-                <span className={`block md:col-span-1`}> <Input state={state} type={`text`} name={`budget`} label={`Budget Range`} textColor={textColor} bgColor={bgColor} /></span>
-                <span className={`block md:col-span-1`}><Input state={state} type={`text`} name={`timing`} label={`Timing for Start`} textColor={textColor} bgColor={bgColor} /></span>
+            {state.errors[0] && 
+                <div className={`bg-[#E10000] text-white py-3 px-6 mt-3 mb-6`}>
+                    {state.errors.map((error) => {
+                        return(
+                            <p className={`${theme.text.P_BLD}`}>{error.message}</p>
+                        )
+                    })}
+                </div>
+            }
+
+            {state.succeeded &&
+                <div className={`text-center text-rm-green`}>
+                    <p className={`${theme.text.P_BLD}`}>Thank you for your submission. We will be in touch.</p>
+                </div>
+            }
+
+            <span className={`mb-6 block md:col-span-1`}><Input state={state} type={`text`} name={`name`} label={`Name`} textColor={textColor} bgColor={bgColor} /></span>
+            <span className={`mb-6 block md:col-span-1`}><Input state={state} type={`text`} name={`company`} label={`Company`} textColor={textColor} bgColor={bgColor} /></span>
+            <span className={`mb-6 block md:col-span-1`}><Input state={state} type={`email`} name={`email`} label={`Email`} textColor={textColor} bgColor={bgColor} /></span>
+            <span className={`mb-6 block md:col-span-1`}><Input state={state} type={`tel`} name={`phone`} label={`Phone`} textColor={textColor} bgColor={bgColor} /></span>
+            <span className={`block md:col-span-1`}> <Input state={state} type={`text`} name={`budget`} label={`Budget Range`} textColor={textColor} bgColor={bgColor} /></span>
+            <span className={`block md:col-span-1`}><Input state={state} type={`text`} name={`timing`} label={`Timing for Start`} textColor={textColor} bgColor={bgColor} /></span>
+
             <div className={`mt-6 ${btnContainerClasses ? btnContainerClasses : ``}`}>
                 <button
                     className={`${theme.button['BASE_STYLING']} ${theme.button[btnStyle ? btnStyle : `SOLID_GREEN_HOVER_DARK`]} cursor-pointer min-w-[210px]`}
