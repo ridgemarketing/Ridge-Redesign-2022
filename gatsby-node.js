cba8af5d1847ca4cb389b43aa9c9fff9913f428f
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2023-05-25 10:29:25 -0400

    add: redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,17 @@
 const path = require(`path`)
+const redirects = require("./src/static/redirects.json")
 const { slash } = require(`gatsby-core-utils`)
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage, createRedirect } = actions
   // query content for WordPress posts
+
+  redirects.forEach(redirect => 
+    createRedirect({
+      fromPath: redirect.fromPath,
+      toPath: redirect.toPath,
+    })
+  )
+
   const {
     data: {
       allWpPost:    { edges: allPosts },
