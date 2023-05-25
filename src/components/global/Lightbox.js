6e2af98c957cfbeef19bd2248e22ba266746c792
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2023-05-25 16:16:16 -0400

    fix: lightbox err

--- src/components/global/Lightbox.js
+++ src/components/global/Lightbox.js
@@ -36,6 +36,7 @@ const LightBox = (props) => {
         if(overlay === true){
             setOverlay(false);
             document.body.classList.remove("overflow-hidden");
+            setImage(0);
         }
     }
     const linkInfo = {
@@ -75,19 +76,23 @@ const LightBox = (props) => {
                 {!video && 
                     <div className={'relative'}>
                         {images[0] &&
-                            <img aria-controls={`image-${image}`} src={images[image].image.publicUrl} alt={``} className={`w-full z-0`} />
+                            <img aria-controls={`image-${image}`} src={images[image] ? images[image].image.publicUrl : images[0].image.publicUrl} alt={``} className={`w-full z-0`} />
                         }
-                       
-                        <button className={`hidden md:block absolute top-1/2 left-[-50px] ml-2 z-50 text-white ${images.length < 3 ? 'hidden' : ''}`} onClick={()=>loadPrev()} onKeyDown={()=>loadPrev()} aria-label="Next Image" tabIndex={0}>
-                            <svg width="35" height="24" viewBox="0 0 35 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M11.9531 22.9375L13.5156 21.375C13.8281 20.9844 13.8281 20.4375 13.4375 20.0469L7.1875 14.0312L34.0625 14.0312C34.6094 14.0312 35 13.5625 35 13.0938V10.9062C35 10.3594 34.6094 9.96875 34.0625 9.96875L7.1875 9.96875L13.4375 3.875C13.8281 3.48437 13.8281 2.9375 13.5156 2.54687L11.9531 0.984375C11.5625 0.671875 11.0156 0.671875 10.625 0.984375L0.3125 11.2969C0 11.6875 0 12.2344 0.3125 12.625L10.625 22.9375C11.0156 23.25 11.5625 23.25 11.9531 22.9375Z" fill="currentColor"/>
-                            </svg>
-                        </button>
-                        <button className={`hidden md:block absolute top-1/2 right-[-50px] z-50 mr-2 text-white ${images.length < 3 ? 'hidden' : ''}`} onClick={()=>loadNext()} onKeyDown={()=>loadNext()} aria-label="Previous Image" tabIndex={0}>
-                            <svg width="35" height="24" viewBox="0 0 35 24" fill="none">
-                                <path d="M23.0469 1.0625L21.4844 2.625C21.1719 3.01562 21.1719 3.5625 21.5625 3.95312L27.8125 9.96875H0.9375C0.390625 9.96875 0 10.4375 0 10.9062V13.0938C0 13.6406 0.390625 14.0312 0.9375 14.0312H27.8125L21.5625 20.125C21.1719 20.5156 21.1719 21.0625 21.4844 21.4531L23.0469 23.0156C23.4375 23.3281 23.9844 23.3281 24.375 23.0156L34.6875 12.7031C35 12.3125 35 11.7656 34.6875 11.375L24.375 1.0625C23.9844 0.75 23.4375 0.75 23.0469 1.0625Z" fill="currentColor"/>
-                            </svg>
-                        </button>
+                        {images.length > 1 &&
+                            <>
+                                <button className={`hidden md:block absolute top-1/2 left-[-50px] ml-2 z-50 text-white ${images.length < 3 ? 'hidden' : ''}`} onClick={()=>loadPrev()} onKeyDown={()=>loadPrev()} aria-label="Next Image" tabIndex={0}>
+                                    <svg width="35" height="24" viewBox="0 0 35 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.9531 22.9375L13.5156 21.375C13.8281 20.9844 13.8281 20.4375 13.4375 20.0469L7.1875 14.0312L34.0625 14.0312C34.6094 14.0312 35 13.5625 35 13.0938V10.9062C35 10.3594 34.6094 9.96875 34.0625 9.96875L7.1875 9.96875L13.4375 3.875C13.8281 3.48437 13.8281 2.9375 13.5156 2.54687L11.9531 0.984375C11.5625 0.671875 11.0156 0.671875 10.625 0.984375L0.3125 11.2969C0 11.6875 0 12.2344 0.3125 12.625L10.625 22.9375C11.0156 23.25 11.5625 23.25 11.9531 22.9375Z" fill="currentColor"/>
+                                    </svg>
+                                </button>
+                                <button className={`hidden md:block absolute top-1/2 right-[-50px] z-50 mr-2 text-white ${images.length < 3 ? 'hidden' : ''}`} onClick={()=>loadNext()} onKeyDown={()=>loadNext()} aria-label="Previous Image" tabIndex={0}>
+                                    <svg width="35" height="24" viewBox="0 0 35 24" fill="none">
+                                        <path d="M23.0469 1.0625L21.4844 2.625C21.1719 3.01562 21.1719 3.5625 21.5625 3.95312L27.8125 9.96875H0.9375C0.390625 9.96875 0 10.4375 0 10.9062V13.0938C0 13.6406 0.390625 14.0312 0.9375 14.0312H27.8125L21.5625 20.125C21.1719 20.5156 21.1719 21.0625 21.4844 21.4531L23.0469 23.0156C23.4375 23.3281 23.9844 23.3281 24.375 23.0156L34.6875 12.7031C35 12.3125 35 11.7656 34.6875 11.375L24.375 1.0625C23.9844 0.75 23.4375 0.75 23.0469 1.0625Z" fill="currentColor"/>
+                                    </svg>
+                                </button>
+                            </>
+                        }
+
                     </div>
                 }
                 {video &&
@@ -96,6 +101,7 @@ const LightBox = (props) => {
                     </div>
                 }
                 <h3 className={`${theme.text.H4} pt-4 text-rm-white text-center`}>{(!video) ? images[image].text : props.title}</h3>
+                
                 <div className={"flex justify-between pt-6 md:hidden w-full"}>
                     <button className={`relative text-white ml-2 z-50 ${images.length < 3 ? 'hidden' : ''}`} onClick={()=>loadPrev()} onKeyDown={()=>loadPrev()} aria-label="Next Image" tabIndex={0}>
                                 <svg width="35" height="24" viewBox="0 0 35 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -108,6 +114,7 @@ const LightBox = (props) => {
                                 </svg>
                     </button>
                 </div>
+               
                 {linkInfo.url !== null && <div className={'pt-10'}>
                         <Link
                             link={linkInfo}
