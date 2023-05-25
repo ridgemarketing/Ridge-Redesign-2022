2af396ffdfbe256fc56ca377f930e6b729ec38a8
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2023-05-25 10:29:18 -0400

    fix: 404 layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,16 +6,14 @@ import { theme } from "../static/theme"
 
 const NotFound = () => {
   return (
-    <Layout>
-          <Container>
-            <div className={`page404`}>
-                <p className={`${theme.text.H4}`}>Page Not Found</p>
-                <h1>We’re sorry, the dog stole your page…</h1>
-                <p className={`${theme.text.P_STD}`}>Finley can be a mischievous little dog when it comes to socks, rocks and web pages, but we’re sure he’ll bring it back soon. In the meantime, please try one of these equally awesome pages instead…</p>
-            </div>
-            <Menu />
-          </Container>
-    </Layout>
+      <Container>
+        <div className={`page404`}>
+            <p className={`${theme.text.H4}`}>Page Not Found</p>
+            <h1>We’re sorry, the dog stole your page…</h1>
+            <p className={`${theme.text.P_STD}`}>Finley can be a mischievous little dog when it comes to socks, rocks and web pages, but we’re sure he’ll bring it back soon. In the meantime, please try one of these equally awesome pages instead…</p>
+        </div>
+        <Menu />
+      </Container>
   )
 }
 
