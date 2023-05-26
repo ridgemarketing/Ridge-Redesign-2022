e966cc3760f940c1e1efa6ea5fec1f42cdf4c849
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2023-05-26 13:43:56 -0400

    refactor: forms to react, sendgrid

--- src/components/global/FormFields.js
+++ src/components/global/FormFields.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react"
 import { theme } from '../../static/theme.js'
-import { ValidationError } from "@formspree/react"
 
-export const Input = ({type, label, name, textColor, state}) => {
+export const Input = ({type, label, name, textColor, required, errors, register}) => {
     const [isFocused, setIsFocused] = useState(false)
     //const [hasValue, setHasValue] = useState(false)
 
@@ -20,14 +19,15 @@ export const Input = ({type, label, name, textColor, state}) => {
             <label className={`block mb-2 ${theme.text['FOOTER']} ${theme.forms['LABEL']} text-rm-${textColor} duration-500 ${isFocused || type === `file` ? `opacity-1` : `opacity-0 translate-y-6`}`} htmlFor={name}>{label}</label>
             <span className={`relative block`}>
                 <span className={`absolute top-0 left-0 w-full transition-all ${theme.text['P_BLD']} duration-300 ${isFocused || type === `file` ? `opacity-0 -translate-y-4` : `opacity-1`}`}>{label}</span>
-                <input required onBlur={checkValue} onFocus={() => setIsFocused(true)} className={`relative w-full ${theme.forms.INPUT} ${theme.text['P_BLD']} form-control bg-transparent border-rm-${textColor} text-rm-${textColor} focus:border-rm-green`} id={name} type={type} name={name} />
-                <ValidationError field={name} prefix={name} errors={state.errors} />
+                <input type={type} onBlur={checkValue} onFocus={() => setIsFocused(true)} {...register(name, { required: required })} className={`relative w-full ${theme.forms.INPUT} ${theme.text['P_BLD']} form-control bg-transparent border-rm-${textColor} text-rm-${textColor} focus:border-rm-green`} />
+                {/* errors will return when field validation fails  */}
+                {errors.name && <span>This field is required</span>}
             </span>
         </>
     )
 }
 
-export const TextArea = ({label, name, textColor, state}) => {
+export const TextArea = ({label, name, textColor, required, errors, register}) => {
     const [isFocused, setIsFocused] = useState(false)
     //const [hasValue, setHasValue] = useState(false)
 
@@ -44,8 +44,8 @@ export const TextArea = ({label, name, textColor, state}) => {
             <label className={`block mb-2 ${theme.text['FOOTER']} ${theme.forms['LABEL']} text-rm-${textColor} duration-500 ${isFocused ? `opacity-1` : `opacity-0 translate-y-6`}`} htmlFor={name}>{label}</label>
             <span className={`relative block`}>
                 <span className={`absolute top-0 left-0 w-full transition-all ${theme.text['P_BLD']} duration-300 ${isFocused ? `opacity-0 -translate-y-4` : `opacity-1`}`}>{label}</span>
-                <textarea onBlur={checkValue} onFocus={() => setIsFocused(true)} className={`relative w-full ${theme.forms.INPUT} ${theme.text['P_BLD']} form-control bg-transparent border-rm-${textColor} text-rm-${textColor} focus:border-rm-green`} id={name} rows={4} name={name} />
-                <ValidationError field={name} prefix={name} errors={state.errors} />
+                <textarea onBlur={checkValue} onFocus={() => setIsFocused(true)} {...register(name, { required: required })} className={`relative w-full ${theme.forms.INPUT} ${theme.text['P_BLD']} form-control bg-transparent border-rm-${textColor} text-rm-${textColor} focus:border-rm-green`} rows={4} />
+                {errors.name && <span class="block">This field is required</span>}
             </span>
         </>
     )
