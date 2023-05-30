64e5d6b1a632a30cabc116e28b52379395baef37
ridgemarketing <84978060+ridgemarketing@users.noreply.github.com>
2023-05-30 11:51:53 -0400

    add: textarea to cta forms

--- src/components/global/Forms.js
+++ src/components/global/Forms.js
@@ -236,9 +236,10 @@ export const FormCTALayout = ({classes, submitLabel, btnContainerClasses, btnSty
             <span className={`mb-6 block md:col-span-1`}><Input errors={errors} register={register} required={true} type={`text`} name={`company`} label={`Company`} textColor={textColor} bgColor={bgColor} /></span>
             <span className={`mb-6 block md:col-span-1`}><Input errors={errors} register={register} required={true} type={`email`} name={`email`} label={`Email`} textColor={textColor} bgColor={bgColor} /></span>
             <span className={`mb-6 block md:col-span-1`}><Input errors={errors} register={register} required={true} type={`tel`} name={`phone`} label={`Phone`} textColor={textColor} bgColor={bgColor} /></span>
-            <span className={`block md:col-span-1`}> <Input errors={errors} register={register} required={true} type={`text`} name={`budget`} label={`Budget Range`} textColor={textColor} bgColor={bgColor} /></span>
-            <span className={`block md:col-span-1`}><Input errors={errors} register={register} required={true} type={`text`} name={`timing`} label={`Timing for Start`} textColor={textColor} bgColor={bgColor} /></span>
-
+            <span className={`mb-6 block md:col-span-1`}> <Input errors={errors} register={register} required={true} type={`text`} name={`budget`} label={`Budget Range`} textColor={textColor} bgColor={bgColor} /></span>
+            <span className={`mb-6 block md:col-span-1`}><Input errors={errors} register={register} required={true} type={`text`} name={`timing`} label={`Timing for Start`} textColor={textColor} bgColor={bgColor} /></span>
+            <span className={`block md:col-span-2`}><TextArea errors={errors} register={register} required={true} name={`message`} label={`What are your goals?`} textColor={textColor} bgColor={bgColor} /></span>
+            
             <div className={`mt-6 ${btnContainerClasses ? btnContainerClasses : ``}`}>
                 <button
                     className={`${theme.button['BASE_STYLING']} ${theme.button[btnStyle ? btnStyle : `SOLID_GREEN_HOVER_DARK`]} cursor-pointer min-w-[210px]`}
