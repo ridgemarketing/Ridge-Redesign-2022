0f0db3e4ca2efdde6673ef8882fdfbb06c91dc69
harrison-ridge <105393490+harrison-ridge@users.noreply.github.com>
2023-06-21 17:09:48 -0400

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         url:
         // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
           process.env.WPGRAPHQL_URL ||
-          `https://rm2022stage.wpengine.com/graphql`,
+          `https://rm2022.wpengine.com/graphql`,
           // `http://ridge-marketing-2022.local/graphql`,
         schema: {
           //Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
