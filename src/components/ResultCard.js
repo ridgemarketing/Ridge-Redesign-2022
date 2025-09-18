95934ec7e82285eb15c3654cf39ce6f7ad122b3b
AlejandroRidge <alejandro@ridgemarketing.com>
2025-09-18 13:58:24 -0400

    fixing counter

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -8,7 +8,10 @@ const ResultCard = (props) => {
 
     const content           = props.content;
     let statClass           = `accent-text font-semibold font-stratos ${props.statFontSize || 'text-[100px] xl:text-[120px]'}`;
-    let descriptionClass    = props.columns === '1' ? 'md:max-w-[60%]' : '';
+    let descriptionClass = `
+        ${props.columns === '1' ? 'md:max-w-[60%]' : ''}
+        ${props.noBoldDescription ? 'font-normal' : ''}
+        `;
     const statNumber        = content.stat ? content.stat.split(/\D+/)[0] : false;
     const suffixString      = content.stat ? content.stat.split(/\d+/)[1] : false;
     const columnsNum        = parseInt(props.columns);
@@ -34,12 +37,19 @@ const ResultCard = (props) => {
     return (
             <div key={`${content.description}${content.stat}`} className={ `text-center md:text-left ${props.columns !== "1" && classes[props.columns]} my-6` }>
                 <div className={`w-fit mx-auto ${props.columns === "1" && classes[props.columns]}`}>
-                {content.stat &&
-                    <span className={statClass}>{content.stat}</span>
-                    }          
-                { content.description &&  
-                <p dangerouslySetInnerHTML={{__html: Parser(content.description)}} className={`${theme.text.H5 + 'font-basic-sans normal-case'} ${descriptionClass} xl:${theme.text.H4} !font-normal`}></p>  
+                {!props.noCounter && content.stat && statNumber > 10 &&
+                    <Counter number={statNumber} title={suffixString} classes={statClass} columns={columnsNum} />   
                 }
+                {!props.noCounter && content.stat && statNumber < 10 &&
+                    <span className={statClass}>{`${statNumber}${suffixString}`}</span>   
+                }          
+                {props.noCounter && content.stat &&
+                    <span className={statClass}>{content.stat}</span>
+                    }   
+                {content.description &&  
+                   <p dangerouslySetInnerHTML={{__html: Parser(content.description)}} className={`${theme.text.H5 + 'font-basic-sans normal-case'} ${descriptionClass}`}></p>  
+                }
+
                 { content.company &&  
                    <p className={`${theme.text.P_STD} mt-4`}>
                         { content.company }
@@ -52,8 +62,8 @@ const ResultCard = (props) => {
                             theme.text_links['BASE_STYLING'] + 
                             theme.text_links['FWD_BASE'] + 
                             theme.text_links['STD'] + 
-                            theme.text_links['ARW_FWD_BLACK'] }> 
-                        VIEW CASE STUDY 
+                            theme.text_links['ARW_FWD_BLACK'] }>
+                        VIEW CASE STUDY
                     </Link>
                 }
                 </div>
