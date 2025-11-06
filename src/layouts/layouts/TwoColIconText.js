6101edac7417a3eeaa0736d90adf978f86df0efc
AlejandroRidge <alejandro@ridgemarketing.com>
2025-11-06 11:23:08 -0500

    fixing parser errors

--- src/layouts/layouts/TwoColIconText.js
+++ src/layouts/layouts/TwoColIconText.js
@@ -36,7 +36,7 @@ const TwoColIconText = (props) => {
                 {/* Text (textarea) */}
                 {item.text && (
                   <p className={theme.text.P_STD + " text-center lg:text-left mb-6"}>
-                    {Parser(item.text)}
+                    {item.text}
                   </p>
                 )}
 
