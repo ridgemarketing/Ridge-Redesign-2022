186e5d21f33809588ee0b2201023d69603b4943c
AlejandroRidge <alejandro@ridgemarketing.com>
2026-01-15 14:12:36 -0500

    meta data fixes - edits to page.js and gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,13 @@
 const React = require("react")
 const Layout = require("./src/components/global/Layout").default
 
+
 // Wraps every page in a component
 exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
+ return <Layout {...props}>{element}</Layout>
 }
+
+
+exports.onRenderBody = ({ setHtmlAttributes }) => {
+ setHtmlAttributes({ lang: "en" });
+};
