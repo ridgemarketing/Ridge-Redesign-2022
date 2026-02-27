1face118c295626b61e5255f68353e08d2b6dea7
harrison-ridge <105393490+harrison-ridge@users.noreply.github.com>
2026-02-27 15:47:58 -0500

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,6 +8,17 @@ exports.wrapPageElement = ({ element, props }) => {
 }
 
 
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
  setHtmlAttributes({ lang: "en" });
+ setHeadComponents([
+  <script key="mouseflow" type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+   window._mfq = window._mfq || [];
+   (function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.defer = true;
+    mf.src = "//cdn.mouseflow.com/projects/872b2e90-96a2-47de-a37c-019092da20c6.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+   })();
+  `}} />
+ ]);
 };
