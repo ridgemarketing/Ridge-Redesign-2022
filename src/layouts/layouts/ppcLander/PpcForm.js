713e94285049ba01fe7e3ad73b36449462c1fadc
harrison-ridge <105393490+harrison-ridge@users.noreply.github.com>
2026-04-07 15:50:45 -0400

    QA: b2b page

--- src/layouts/layouts/ppcLander/PpcForm.js
+++ src/layouts/layouts/ppcLander/PpcForm.js
@@ -21,12 +21,12 @@ const PPCForm = ({data, cmo, persistantEmail}) => {
                 <BottomCloudPiece_Medium className={`w-full hidden md:block xl:hidden cloudAnimation-Bottom z-10 relative`} />
                 <BottomCloudPiece_Small className={`w-full md:hidden cloudAnimation-Bottom z-10 relative`} />
             </>}
-            <div className={`pb-[160px] relative ${cmo ? `pt-[160px] bg-[#f3f1ee] bg-[linear-gradient(225deg,rgba(0,171,182,0.25)_0%,transparent_35%,transparent_65%,rgba(0,171,182,0.25)_100%)]` : `bg-gradient-to-t from-white via-[#f3f9f9] via-47% to-[#edf8f9]` } z-20`}>
+            <div className={`pb-[160px] relative ${cmo ? `pt-10 md:pt-[160px] bg-[#f3f1ee] bg-[linear-gradient(225deg,rgba(0,171,182,0.25)_0%,transparent_35%,transparent_65%,rgba(0,171,182,0.25)_100%)]` : `bg-gradient-to-t from-white via-[#f3f9f9] via-47% to-[#edf8f9]` } z-20`}>
                 <Container classes={`flex flex-col gap-[100px]`}>
                     <div className="flex flex-col-reverse items-center justify-center xl:flex-row xl:flex-wrap flex-nowrap xl:gap-6">
                         <div className="flex flex-col gap-6 text-left flex-1">
                             {heading &&
-                                <h2 id="form" dangerouslySetInnerHTML={{__html: Parser(heading)}} className={`pt-20 text-center xl:text-left text-[2.5rem] leading-[2.7rem] md:text-[4.5rem] md:leading-[4.475rem] font-stratos font-semibold text-black uppercase`}></h2>
+                                <h2 id="form" dangerouslySetInnerHTML={{__html: Parser(heading)}} className={`${cmo ? 'pt-8 md:pt-20' : 'pt-20'} text-center xl:text-left text-[2.5rem] leading-[2.7rem] md:text-[4.5rem] md:leading-[4.475rem] font-stratos font-semibold text-black uppercase`}></h2>
                             }
                             {body &&
                                 <p dangerouslySetInnerHTML={{__html: Parser(body)}} className={`text-center xl:text-left ${theme.text.H4_LTE} text-rm-grey max-w-[835px] mx-auto`}></p>
@@ -41,7 +41,7 @@ const PPCForm = ({data, cmo, persistantEmail}) => {
                             <img 
                                 src         = {image.sourceUrl} 
                                 alt         = {image.altText} 
-                                className   = {`flex xl:self-start w-[459px] h-[459px] object-contain`} />
+                                className   = {`flex xl:self-start max-w-[250px] md:max-w-[459px] md:w-[459px] md:h-[459px] object-contain`} />
                         }
                     </div>
                     <div className="flex flex-col gap-4">
